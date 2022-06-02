18b2f30fe745613f16f44c1c04ecda630a8ac85d
Alex <Alex>
2022-06-02 23:13:57 +0700

    Initial commit

---
+++ src/test/java/ru/example/alfatest/AlfaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.example.alfatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlfaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
